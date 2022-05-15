2409ac8b8e2dd2cb16b50cd984e440a5e60737cd
kiennd <kienhyym@gmail.com>
2022-05-15 13:57:01 +0700

    update course

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -1,5 +1,9 @@
 const Courses = require('../model/Course')
-const { mongooseToObject } = require('../../util/mongoose')
+const {
+    mongooseToObject,
+    mutipleMongooseToObject,
+} = require('../../util/mongoose')
+const Course = require('../model/Course')
 
 class CoursesController {
     index(req, res, next) {
@@ -19,6 +23,31 @@ class CoursesController {
         course
             .save()
             .then(() => res.redirect('/'))
+            .catch(next)
+    }
+    list(req, res, next) {
+        Course.find({})
+            .then((data) => {
+                res.render('course/my_courses', {
+                    courses: mutipleMongooseToObject(data),
+                })
+            })
+            .catch(next)
+    }
+    detail(req, res, next) {
+        Courses.findOne({ slug: req.params.slug })
+            .then((data) => {
+                console.log(mongooseToObject(data))
+                return res.render('course/detail', {
+                    course: mongooseToObject(data),
+                })
+            })
+            .catch(next)
+    }
+    update(req, res, next) {
+        const formdata = req.body
+        Course.updateOne({ slug: req.params.slug }, formdata)
+            .then(() => res.redirect('/course/my_courses'))
             .catch(next)
     }
 }
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,7 +4,10 @@ const router = express.Router()
 const coursesController = require('../app/controllers/CoursesController')
 
 router.get('/create', coursesController.create)
+router.get('/my_courses', coursesController.list)
 router.post('/store', coursesController.store)
+router.post('/detail/:slug/update', coursesController.update)
 router.get('/:slug', coursesController.index)
+router.get('/detail/:slug', coursesController.detail)
 
 module.exports = router
