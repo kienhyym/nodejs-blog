aa675067f0fe175e8290d6d82bbcdc97de82f813
kiennd <kienhyym@gmail.com>
2022-05-28 02:06:32 +0700

    update method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,15 +1,66 @@
 const User = require('../model/User')
 
-const UserControler = async (req, res) => {
+const addUser = async (req, res) => {
     try {
         const user = new User(req.body)
-        const newUser = await user.save()
-        console.log('save success', user)
-        return res.send(user)
+        await user.save()
+        return res.status(200).send(user)
     } catch (error) {
         return res.send(error)
     }
 }
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({})
+        return res.status(200).send(users)
+    } catch (error) {
+        return res.send(error)
+    }
+}
+const getUserbyID = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.send(error)
+    }
+}
+const updateUserbyID = async (req, res) => {
+    try {
+        const updates = Object.keys(req.body)
+        const allowUpdate = ['name', 'email', 'password', 'age']
+        isValidate = updates.every((item) => allowUpdate.includes(item))
+        if (!isValidate) {
+            return res.status(404).send({ error: 'param isvalidate' })
+        }
+
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if (!user) {
+            return res.status(404).send({ error: 'user not found ' })
+        }
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+}
+const deleteUserbyID = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send({ error: 'user not found ' })
+        }
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+}
 module.exports = {
-    UserControler,
+    addUser,
+    getAllUsers,
+    getUserbyID,
+    updateUserbyID,
+    deleteUserbyID,
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { UserControler } = require('../app/controllers/UserController')
+const {
+    addUser,
+    getAllUsers,
+    getUserbyID,
+    updateUserbyID,
+    deleteUserbyID,
+} = require('../app/controllers/UserController')
 
-router.post('/', UserControler)
+router.delete('/:id', deleteUserbyID)
+router.patch('/:id', updateUserbyID)
+router.get('/users/:id', getUserbyID)
+router.get('/users', getAllUsers)
+router.post('/', addUser)
 
 module.exports = router
